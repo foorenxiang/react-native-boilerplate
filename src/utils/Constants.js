bc94954cb9b0aa4b14f0af9c6a853e5390971003
Ren Xiang <foorenxiang@gmail.com>
2021-08-27 03:54:25 +0800

    change app identifier

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
 export const APP_VERSION = 0.68;
 export const MIN_SUPPORTED_DEVICE_YEAR = 2015;
 export const ERROR_USER_EXIST = 'User exists!';
-export const PERSIST_KEY_REGISTERED_USERS = 'EWASTE::REGISTERED_USERS';
-export const PERSIST_KEY_ACTIVE_PROFILE = 'EWASTE::ACTIVE_PROFILE';
+export const PERSIST_KEY_REGISTERED_USERS = 'ReactNativeBoilerplate::REGISTERED_USERS';
+export const PERSIST_KEY_ACTIVE_PROFILE = 'ReactNativeBoilerplate::ACTIVE_PROFILE';
